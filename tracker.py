94aea17fe3115f238c930a354ddf035815fa8f96
Vlad-Mihai Moldovan <vladmoldovan56@gmail.com>
2021-01-08 18:16:51 +0200

    STOCKERS-DEV-001 - code refactoring; support for computing trend, box, breakout and stage - WIP

--- tracker.py
+++ tracker.py
@@ -2,11 +2,13 @@ import argparse
 import os, time
 import smtplib, ssl
 import pandas as pd
+import numpy as np
 import yfinance as yf
 from gsheets import Sheets
 from datetime import datetime
 from getpass import getpass
 from email.message import EmailMessage
+from tabulate import tabulate
 
 STOCKERS_URL = 'https://docs.google.com/spreadsheets/d/1zEt_CQo8RI808xXFARTPvcTD7Rop_9zQ5Uw8Uq-Ywmk/edit#gid=0'
 
@@ -14,19 +16,22 @@ INFO_STATS = ['previousClose','regularMarketOpen','regularMarketDayHigh','regula
 			'morningStarRiskRating','morningStarOverallRating','regularMarketPrice','dayHigh','dayLow',
 			'fiftyTwoWeekLow','fiftyTwoWeekHigh','exDividendDate','fiftyDayAverage','twoHundredDayAverage',
 			'dividendRate','trailingAnnualDividendRate']
-RECEIVERS = ['vladmoldovan56@gmail.com','octavbirsan@gmail.com', 'adrian_steau@yahoo.com', 'sirbu96vlad@gmail.com', 'bogdanrogojan96@gmail.com', 'barb.alin.gabrial.pp@gmail.com']
+RECEIVERS = ['vladmoldovan56@gmail.com','octavbirsan@gmail.com', 'adrian_steau@yahoo.com', 'sirbu96vlad@gmail.com', 'bogdanrogojan96@gmail.com', 'barb.alin.gabriel.pp@gmail.com']
+RECEIVERS_SHORTLIST = ['vladmoldovan56@gmail.com', 'adrian_steau@yahoo.com', 'sirbu96vlad@gmail.com']
 
 
 CREDENTIALS_JSON = './credentials.json'
 STORAGE_JSON = './storage.json'
 CONFIG_FILE = '.config'
 
-def get_parser():
-	parser = argparse.ArgumentParser(description='Process Stockers Tracker:P')
-	parser.add_argument('profile', type=string, nargs='',default='all'
-                    help='Value for members profiles')
+# def get_parser():
+# 	parser = argparse.ArgumentParser(description='Process Stockers Tracker:P')
+# 	parser.add_argument('profile', type=string, nargs='',default='all'
+#                     help='Value for members profiles')
 
-	print(parser)
+# 	print(parser)
+
+
 def get_credentials(account='email'):
 	with open(CONFIG_FILE) as f:
 		lines = f.readlines()
@@ -41,12 +46,33 @@ def get_credentials(account='email'):
 	return user, pw
 
 
+def send_mail_alert(alert_message):
+	time_now = datetime.now().isoformat(' ', 'seconds')
+
+	sender, password = get_credentials()
+
+	msg = EmailMessage()
+	msg.set_content(alert_message)
+	msg['Subject'] = f"{time_now}: Stockers signals for today."
+	msg['From'] = sender
+	msg['To'] = RECEIVERS_SHORTLIST
+
+	context = ssl.create_default_context()
+	with smtplib.SMTP_SSL("smtp.gmail.com", port=465, context=context) as server:
+		server.login(sender, password)
+		server.send_message(msg)
+		print("Email alert sent successfully!")
+
+
 def compute_ema(df, days):
 	return round(df['Close'].ewm(span=days, adjust=False).mean(), 2)
 
 
-def compute_sma(df, days):
-	return round(df['Close'].rolling(window=days).mean(), 2)
+def compute_sma(df, days, shifted=False):
+	if shifted:
+		return df['Close'].shift().rolling(window=days).mean()
+	else:	
+		return round(df['Close'].rolling(window=days).mean(), 2)
 
 
 def compute_rsi(df, time_window=14):
@@ -72,33 +98,57 @@ def compute_macd(df, fast_period=12, slow_period=26, signal_period=9):
 	slow_ema = compute_ema(df, slow_period)
 	macd = fast_ema - slow_ema
 	
-	df2 = df.copy()
-	df2['Close'] = macd
-	signal_ema = compute_ema(df2, signal_period)
+	df_copy = df.copy()
+	df_copy['Close'] = macd
+	signal_ema = compute_ema(df_copy, signal_period)
 	
 	macd_histo = round(macd - signal_ema, 3)
 
 	return macd_histo
 
 
-def send_mail_alert(alert_message):
+def compute_trend(df):
+	conditions = [
+		(df['EMA_5'] > df['EMA_10']) & (df['EMA_10'] > df['SMA_20']),
+		(df['EMA_5'] < df['EMA_10']) & (df['EMA_10'] < df['SMA_20']),
+	]
+	values =['\u279A', '\u2798']
+	return np.select(conditions, values, default = '\u2799')
 
-	time_now = datetime.now().isoformat(' ', 'seconds')
 
-	sender, password = get_credentials()
-	receivers = RECEIVERS
+def compute_box(df, trailing_days=4, threshold_percentage=0.04):
+	# average_price_for_trailing_days = compute_sma(df, 4, shifted=True)
+	conditions = [
+		# abs(df['Close'] - average_price_for_trailing_days) > (threshold_percentage * average_price_for_trailing_days), 
+		# abs(df['Close'] - average_price_for_trailing_days) < (threshold_percentage * average_price_for_trailing_days), 
+		abs(df['Close'] - df['EMA_5']) > (threshold_percentage * df['EMA_5']), 
+		abs(df['Close'] - df['EMA_5']) < (threshold_percentage * df['EMA_5']), 
+	]
+	values = ['OUT', 'IN']
+	
+	return np.select(conditions, values)
 
-	msg = EmailMessage()
-	msg.set_content(alert_message)
-	msg['Subject'] = f"{time_now}: Stockers signals for today."
-	msg['From'] = sender
-	msg['To'] = receivers
+def compute_break_sma_20(df):
+	conditions = [
+		(df['Close'] > df['SMA_20']) & (df['Close'].shift() < df['SMA_20'].shift()),
+		(df['Close'] < df['SMA_20']) & (df['Close'].shift() > df['SMA_20'].shift()),
+	]
+	values = ['\u279A', '\u2798']
 
-	context = ssl.create_default_context()
-	with smtplib.SMTP_SSL("smtp.gmail.com", port=465, context=context) as server:
-		server.login(sender, password)
-		server.send_message(msg)
-		print("Email alert sent successfully!")
+	return np.select(conditions, values, default='')
+
+## WIP
+def compute_stage(df):
+	conditions = [
+		(df['Break_20'] == '\u279A') | ((df['Box'] == 'OUT') & (df['Close'] > df['SMA_20']) & (df['Trend'] != '\u2798')),
+		(df['Break_20'] == '\u2798') | ((df['Box'] == 'OUT') & (df['Close'] < df['SMA_20']) & (df['Trend'] != '\u279A')),
+		(df['Box'] == 'IN') & (df['RSI'] < 50),
+		(df['Box'] == 'IN') & (df['RSI'] > 50),
+		# (df['Break_20'] == '\u2798') | (df['Close'] < df['SMA_20'] & df['Box'] == 'OUT'),
+	]
+	values = [2, 4, 1, 3]
+	
+	return np.select(conditions, values, default='???')
 
 
 def compute_ticker_df(ticker=None, period='max'):
@@ -110,40 +160,34 @@ def compute_ticker_df(ticker=None, period='max'):
 	try:
 		ticker = yf.Ticker(ticker)
 		df = ticker.history(period=period)
-		
-		df['Ticker'] = ticker.ticker
-		df['EMA_5'] = compute_ema(df, 5)
-		# df['MA_5'] = compute_sma(df, 5)
-		df['MA_10'] = compute_sma(df, 10)
-		df['MA_20'] = compute_sma(df, 20)
-		df['MA_200'] = compute_sma(df, 200)
-		df['RSI'] = compute_rsi(df, 14)
-		df['MACD'] = compute_macd(df)
-
-		# print(df.loc[df['Dividends'] != 0.0])
-		# df = df.drop(columns=['Volume', 'Stock Splits'])
 	
-		df = df.reset_index().set_index('Ticker')
-		# df = df[['Date', 'Open', 'Close', 'RSI', 'MA_5', 'MA_20']]
-	
-		return df
-
 	except:
 		print(f"Ticker {ticker} failed!")
 		return pd.DataFrame()
+	
+	else:
+		df['Ticker'] = ticker.ticker
+		df['EMA_5'] = compute_ema(df, 5)
+		df['EMA_10'] = compute_ema(df, 10)
+		df['SMA_20'] = compute_sma(df, 20)
+		df['SMA_200'] = compute_sma(df, 200)
+		df['RSI'] = compute_rsi(df, 14)
+		df['MACD'] = compute_macd(df)
+		# df['prev4SMA'] = compute_sma(df, 4, shifted=True)
+		df['Box'] = compute_box(df)
+		df['Trend'] = compute_trend(df)
+		df['Break_20'] = compute_break_sma_20(df)
+		df['Stage'] = compute_stage(df)
 
-def get_tickers(url, sheet):
-	 sheets = Sheets.from_files(CREDENTIALS_JSON, STORAGE_JSON)
-	 worksheet = sheets.get(url).find(sheet)
-	 df_worksheet = worksheet.to_frame()
 
-	 return df_worksheet['TICKER'].tolist()
-
+		# df = df.drop(columns=['Volume', 'Stock Splits'])
+		df = df.reset_index().set_index('Ticker')
+	
+		return df
 
 def compute_last_day_market_df(ticker_list):
 	# COLUMN_NAMES = ['Date', 'Ticker', 'Close', 'RSI', 'MACD', 'MA_5', 'MA_20', 'MA_200']
 	# df_market = pd.DataFrame(columns=COLUMN_NAMES)
-	
 	df_market = pd.DataFrame()
 
 	for ticker in ticker_list:
@@ -157,122 +201,75 @@ def compute_last_day_market_df(ticker_list):
 	else:
 		return pd.DataFrame()
 
-
-def get_breakdowns_breakouts(ticker, tickers_to_manually_check, message):
-
-	print(f"Checking ticker {ticker}!")
-	df_ticker = compute_ticker_df(ticker=ticker)
-
-	last_day = df_ticker.iloc[-1]
+## obsolete
+def get_bo_bd(ticker, df_ticker, tickers_to_manually_check, message):
+	current_day = df_ticker.iloc[-1]
 	previous_day = df_ticker.iloc[-2]
 
-
-	three_smallest = df_ticker.nsmallest(3, 'Close')
-	second_min = float(three_smallest.head(2).tail(1).Close)
-	# print(f"{ticker} with SECOND_MIN= {second_min}, ")
-	# print(f"ALST DAY CLOSE {type(last_day.Close)}")
-	if last_day.Close <= second_min:
+	# breakout
+	if previous_day.Close < previous_day['SMA_20'] and current_day.Close > current_day['SMA_20']:
 		tickers_to_manually_check.add(ticker)
-		message.append(f"{ticker} just closed lower or equal than second min ({last_day.Close} <= {second_min}")
-		print(f"{ticker} just closed lower or equal than second min ({last_day.Close} <= {second_min}")
+		message.append(f"{ticker} breaking out above the 20-day SMA!")
+		print(f"{ticker} breaking out above the 20-day SMA!")
 
-	#fake close above (2 days in a row above)
-	if previous_day['Close'] > previous_day['MA_20'] and last_day['Open'] < last_day['MA_20'] and last_day['Close'] > last_day['MA_20'] :
+	# breakdown
+	if previous_day.Close > previous_day['SMA_20'] and current_day.Close < current_day['SMA_20']:
 		tickers_to_manually_check.add(ticker)
-		message.append(f"{ticker}: FAKE BREAKOUT!!! PRICE JUST CLOSED ABOVE THE MA_20.")
-		print(f"{ticker}: FAKE BREAKOUT!!! PRICE JUST CLOSED ABOVE THE MA_20.")
-	
-	#opened and closed above
-	if previous_day['Close'] < previous_day['MA_20'] and last_day['Open'] > last_day['MA_20'] and last_day['Close'] > last_day['MA_20']:
-		tickers_to_manually_check.add(ticker)
-		message.append(f"{ticker}: BREAKOUT!! PRICE OPENED AND CLOSED ABOVE THE MA_20.")
-		print(f"{ticker}: BREAKOUT!! PRICE OPENED AND CLOSED ABOVE THE MA_20.")
+		message.append(f"{ticker} breaking down below the 20-day SMA!")
+		print(f"{ticker} breaking down below the 20-day SMA!")
 
-	#opened below closed above
-	if previous_day['Close'] < previous_day['MA_20'] and last_day['Open'] < last_day['MA_20'] and last_day['Close'] > last_day['MA_20']:
-		tickers_to_manually_check.add(ticker)
-		message.append(f"{ticker}: BREAKOUT!! PRICE OPENED BELOW AND CLOSED ABOVE THE MA_20.")
-		print(f"{ticker}: BREAKOUT!! PRICE OPENED BELOW AND CLOSED ABOVE THE MA_20.")
-
-
-	#fake close below
-	if previous_day['Close'] < previous_day['MA_20'] and last_day['Open'] > last_day['MA_20'] and last_day['Close'] < last_day['MA_20']:
-		tickers_to_manually_check.add(ticker)
-		message.append(f"{ticker}: FAKE BREAKDOWN!!!PRICE JUST CLOSED BELOW THE MA_20.")
-		print(f"{ticker}: FAKE BREAKDOWN!!!PRICE JUST CLOSED BELOW THE MA_20.")
-	
-	#opened above closed below
-	if previous_day['Close'] > previous_day['MA_20'] and last_day['Open'] > last_day['MA_20'] and last_day['Close'] < last_day['MA_20']:
-		tickers_to_manually_check.add(ticker)
-		message.append(f"{ticker}: BREAKDOWN!! PRICE OPENED ABOVE AND CLOSED BELOW THE MA_20.")
-		print(f"{ticker}: BREAKDOWN!! PRICE OPENED ABOVE AND CLOSED BELOW THE MA_20.")
-
-	#opened and closed below
-	if previous_day['Close'] > previous_day['MA_20'] and last_day['Open'] < last_day['MA_20'] and last_day['Close'] < last_day['MA_20']:
-		tickers_to_manually_check.add(ticker)
-		message.append(f"{ticker}: BREAKDOWN!! PRICE OPENED AND CLOSED BELOW THE MA_20.")
-		print(f"{ticker}: BREAKDOWN!! PRICE OPENED AND CLOSED BELOW THE MA_20.")
-
-
-	if last_day['RSI'] < 30 and previous_day['RSI'] > 30:
-		tickers_to_manually_check.add(ticker)
-		message.append(f"{ticker}: RSI JUST CLOSED BELOW 30!!!!")
-		print(f"{ticker}: RSI JUST CLOSED BELOW 30!!!!")
-
-	if last_day['RSI'] > 70 and previous_day['RSI'] < 70:
-		tickers_to_manually_check.add(ticker)
-		message.append(f"{ticker}: RSI JUST CLOSED ABOVE 70!!!!")
-		print(f"{ticker}: RSI JUST CLOSED ABOVE 70!!!!")
-
-
+	# ex-dividend date (deocamdata nu merge, are delay de 1 zi :()
 	if df_ticker.iloc[-1]['Dividends']:
 		tickers_to_manually_check.add(ticker)
 		message.append(f"{ticker}: EX DIVIDEND DATE!!!")
 		print(f"{ticker}: EX DIVIDEND DATE!!!")
 
+	return tickers_to_manually_check, message
 
-	return tickers_to_manually_check, message 
+
+def get_tickers(url, sheet):
+	 sheets = Sheets.from_files(CREDENTIALS_JSON, STORAGE_JSON)
+	 worksheet = sheets.get(url).find(sheet)
+	 df_worksheet = worksheet.to_frame()
+
+	 return df_worksheet['TICKER'].tolist()
 
 
 def main():
-	## aici imi construiesc market_df
+	tickers_to_manually_check = set()
+	alert_message = []
 
-	argp = get_parser()
-	sheet = argp['profile']
+	## aici imi iau lista de tickere de pe gsheet in functie de profil
+	# argp = get_parser()
+	# sheet = argp['profile']
+	sheet = "Moldo_Watchlist"
 	ticker_list = get_tickers(STOCKERS_URL, sheet)
 
-	if os.path.getmtime('last_day.csv') < time.time() - 12 * 3600:
-		df_market = compute_last_day_market_df(ticker_list)
-	else:
+	## aici imi construiesc market_df
+	if os.path.isfile('last_day.csv') and os.path.getmtime('last_day.csv') > time.time() - 12 * 3600:
 		df_market = pd.read_csv('last_day.csv')
+	else:
+		df_market = compute_last_day_market_df(ticker_list)
 
 	if not df_market.empty:
 		df_market.to_csv('last_day.csv')
-	##
 
-	df_market = compute_last_day_market_df(ticker_list)
 	print(df_market)
-
-	# df_market = df_market.head(20).append(df_market.tail(20))
-
-	## aici imi verific fiecare ticker
-	tickers_to_manually_check = set()
-	breakdown_tickers = set()
-	breakout_tickers = set()
-	alert_message = []
-
-	for ticker in ticker_list:
-		get_breakdowns_breakouts(ticker, tickers_to_manually_check, alert_message) 
-
 
 	## aici imi construiesc alerta
 	tickers_to_manually_check = ', '.join(tickers_to_manually_check)
 	alert_message = '\n'.join(map(str, alert_message))
-	alert_message = 'Tickers you should check: ' + tickers_to_manually_check + '\n\n' + alert_message + '\n\n' + df_market.to_string()
+	alert_message = 'Tickers you should check: ' + tickers_to_manually_check + '\n\n' + alert_message + '\n\n' + tabulate(df_market, headers='keys', tablefmt='psql')
 
-	# print(alert_message)
-	send_mail_alert(alert_message=alert_message)
+	print(alert_message)
+	# send_mail_alert(alert_message=alert_message)
 	
 	
 if __name__ == '__main__':
 	main()
+	# tickers_to_manually_check = set()
+	# alert_message = []
+	# ticker = 'BYND'
+	# df_ticker = compute_ticker_df(ticker)
+	# print(df_ticker[150:210])
+	# print(df_ticker[-100:])
